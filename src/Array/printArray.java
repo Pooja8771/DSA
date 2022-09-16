6546eee7705051726a2da6c2f26c8f8a750c0059
Pooja8771 <pooja.singh8771@gmail.com>
2022-09-16 15:42:03 +0530

    Initial commit

---
+++ src/Array/printArray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class printArray {
+    public static void main(String[] args) {      
+        int[] arr= new int[]{4,6,8,0,10,9,5};
+        int n = arr.length;
+        for(int i=0;i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
